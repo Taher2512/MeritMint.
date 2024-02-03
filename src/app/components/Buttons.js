ee65304ca1f90225695db18cbd081a8dca74573d
Taher <itscross123@gmail.com>
2024-02-03 15:53:35 +0530

    Fixed an import bug

--- src/app/components/Buttons.js
+++ src/app/components/Buttons.js
@@ -66,7 +66,7 @@ function Buttons({ fetchResults, field, setField, fcl }) {
         </motion.ul>
       </motion.div>
       <button className="px-6 py-2 font-medium bg-indigo-500 text-white w-fit transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]">
-        End Sem
+        End Term
       </button>
       <div>
         <button
