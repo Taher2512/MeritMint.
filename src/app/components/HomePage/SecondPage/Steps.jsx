2e7746c25b969abd186ce447cb52aa6aa8071796
Tarif-dev <tarifhussain212@gmail.com>
2024-02-03 16:20:11 +0530

    Changed Home Page UI

--- src/app/components/HomePage/SecondPage/Steps.jsx
+++ src/app/components/HomePage/SecondPage/Steps.jsx
@@ -4,14 +4,16 @@ function Steps() {
   return (
     <>
       <section className=" w-screen h-auto">
-        <h1 className="gilroy-bold text-slate-300 font-extrabold text-4xl ml-40 mt-32">
+        <h1 className="gilroy-bold text-slate-300 font-extrabold text-4xl ml-36 mb-8 mt-32">
           How it works
         </h1>
         <div className="flex">
-          <div></div>
-          <div className="h-auto w-1 bg-gradient-to-b from-blue-700 via-blue-800 to-gray-900 ml-40 mt-7"></div>
+          <div className="flex flex-col ml-40 items-center">
+            <div className="absolute bg-[#0F172A] w-9 h-8 rounded-full border-4 border-blue-700"></div>
+            <div className="h-full w-1 bg-gradient-to-b from-blue-700 via-blue-800 to-gray-900"></div>
+          </div>
           <ul className="w-2/4 flex flex-col gap-16 mt-8 mb-16 mr-0">
-            <li >
+            <li>
               <div className="w-full px-8 py-5 text-white rounded-2xl border-b-2 border-blue-700">
                 <h2 className="gilroy-bold text-2xl ">
                   Step 1: Achieve & Earn
@@ -26,21 +28,23 @@ function Steps() {
             </li>
             <li>
               <div className="w-full px-8 py-5 text-white rounded-2xl border-b-2 border-blue-800">
-                <h2 className="gilroy-bold text-2xl ">
-                Step 2: Track & Grow
-                </h2>
+                <h2 className="gilroy-bold text-2xl ">Step 2: Track & Grow</h2>
                 <p className="mt-4 ml-4">
-                Our intuitive dashboard allows students and educators to track progress, set goals, and visualize achievements in real-time. Watch your MeritMint balance grow as you reach new heights
+                  Our intuitive dashboard allows students and educators to track
+                  progress, set goals, and visualize achievements in real-time.
+                  Watch your MeritMint balance grow as you reach new heights
                 </p>
               </div>
             </li>
             <li>
               <div className="w-full px-8 py-5 text-white rounded-2xl border-b-2 border-blue-950">
                 <h2 className="gilroy-bold text-2xl ">
-                Step 3: Redeem & Reinvest
+                  Step 3: Redeem & Reinvest
                 </h2>
                 <p className="mt-4 ml-4">
-                MeritMint tokens aren't just symbolic. Redeem them for a range of rewards, from educational resources to exclusive experiences, or save them as an investment in your future.
+                  MeritMint tokens aren't just symbolic. Redeem them for a range
+                  of rewards, from educational resources to exclusive
+                  experiences, or save them as an investment in your future.
                 </p>
               </div>
             </li>
