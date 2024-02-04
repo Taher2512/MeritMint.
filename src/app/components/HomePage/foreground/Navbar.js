cac356ba8e7f11a7a654beee376b593ae2e35c63
Taher <itscross123@gmail.com>
2024-02-04 11:10:05 +0530

    Added token transfer functionality

--- src/app/components/HomePage/foreground/Navbar.js
+++ src/app/components/HomePage/foreground/Navbar.js
@@ -20,8 +20,7 @@ function Navbar() {
 
   const logIn = async () => {
     fcl.authenticate();
-    console.log(fcl.currentUser());
-    setupCollection();
+    console.log("Services", user.services);
   };
   async function setupCollection() {
     console.log("Fcl authz", fcl.authz);
@@ -56,8 +55,14 @@ function Navbar() {
   }
 
   useEffect(() => {
-    fcl.currentUser().subscribe(setUser);
-  }, []);
+    if (user.services) {
+      console.log("Services", user.services[0].scoped.email);
+    }
+
+    if (!user) {
+      fcl.currentUser().subscribe(setUser);
+    }
+  }, [user]);
 
   return (
     <>
@@ -114,6 +119,19 @@ function Navbar() {
           >
             Demo
           </button>
+
+          {user.loggedIn && (
+            <button
+              type="button"
+              onClick={() => {
+                fcl.unauthenticate();
+                localStorage.setItem("Vault", "");
+              }}
+              className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+            >
+              Logout
+            </button>
+          )}
         </span>
       </div>
     </>
