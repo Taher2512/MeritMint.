74f584c902314f374094a8d2d7de69830a606194
Tarif-dev <tarifhussain212@gmail.com>
2024-02-07 11:09:17 +0530

    Did some optimizations

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,18 +3,18 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+      <footer class="bg-white/30 rounded-lg shadow dark:bg-gray-900 m-4">
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div class="sm:flex sm:items-center sm:justify-between">
             <a
               href="#"
               class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
             >
-              <span class="gilroy-bold self-center text-2xl whitespace-nowrap dark:text-white">
+              <span class="gilroy-bold self-center text-2xl whitespace-nowrap text-white">
                 MeritMint.
               </span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul class="flex gilroy-light flex-wrap items-center mb-6 text-sm font-medium text-white">
               <li>
                 <a href="#HowItWorks" class="hover:underline me-4 md:me-6">
                   How it works
@@ -38,7 +38,7 @@ function Footer() {
             </ul>
           </div>
           <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span class="block text-sm text-white sm:text-center">
             © 2023{" "}
             <a href="#" class="hover:underline">
               MeritMint™
